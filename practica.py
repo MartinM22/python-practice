c053df2876541bf9ec6c29c282314fafdf0ca92a
MartinM22 <martinmaldonado1999@gmail.com>
2023-02-28 01:37:08 +0000

    Primeras anotaciones

---
+++ practica.py
@@ -0,0 +1,73 @@
+"""
+Comentario multi línea
+"""
+# comentario de una sola línea
+
+# basic syntax
+"""
+function nombreFunc () {
+
+}
+"""
+
+
+def nombreFunc():
+    """ """
+    """ """
+    """ """
+    """ """
+    """ """
+    """ """
+    """ """
+    return
+
+# Variables
+
+
+nombre = "Martin"
+print(nombre)
+
+# trabajaremos con: snake_case
+
+# Data types
+
+numero_int = 23
+numero_float = 2.5
+numero_float_2 = 2.000000
+
+# Strings
+texto = "Hola"
+texto_2 = 'Hola 2'
+
+# None = null o undefined
+
+no_existe = None
+
+# Booleanos
+
+algo_verdadero = True
+algo_falso = False
+
+# Array
+my_array = [algo_verdadero, "Hola", 23, None]
+
+
+# Objects
+
+class Car():
+    """ """
+
+
+# Diccionario - dict = objetos en JS
+"""
+const user = {
+    nombre : "Martin",
+    edad: 23
+}
+"""
+
+user = {
+    "nombre": "Martin",
+    "edad": 23
+}
+user["nombre"]
